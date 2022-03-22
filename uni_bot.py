902b5c237f5b2e5dc42a68600b647d175632a301
Ari Bermeki <ari.bermeki@icloud.com>
2022-03-22 02:56:05 +0100

    telegram-bot

--- uni_bot.py
+++ uni_bot.py
@@ -1,9 +1,24 @@
+from asyncio import exceptions
+from logging import exception
 import os
+from time import sleep
+from turtle import update
 import requests
 
 
 HTTP_API = os.environ['HTTP_API']
 
+global empfangURL 
+global sendURL 
+global OFFSET
+ 
+
+
+
+
+empfangURL = "http://api.telegram.org/bot" + HTTP_API + "/getUpdates"
+sendUrl = "http://api.telegram.org/bot" + HTTP_API + "/sendMessage"
+OFFSET = 0
 
 
 
@@ -24,36 +39,50 @@ Puede encontrar su ID en su backoffice en el tablero de instrumentos.
 للبدء ، يرجى الرد باستخدام اسم المستخدم المسجل لدينا
 "يمكنك العثور على اسم المستخدم الخاص بك في المكتب الخلفي الخاص بك في القائمة الرئيسية.
 """
+def willkomen(url):
+    global OFFSET
 
 
+    try:
+        request_abfrage_system = requests.get(url + "?offset=" + str(OFFSET))
+        abfrage = request_abfrage_system.json()
+        data_laden = nachricht_extrahieren(abfrage)
+        text = data_laden['message']['text']
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if text != False:
         
 
+            OFFSET = data_laden['update_id'] + 1
+            chat_id = data_laden['message']['chat']['id']
+            nachricht_senden(chat_id, text)
+            
+    except requests.exceptions.ConnectionError:
+        pass
 
 
-    
-if __name__=="__main__":
-    pass
+
+def nachricht_extrahieren(dict):
+    gesamt_array_abrufen = dict['result']
+    message_array_abrufen = gesamt_array_abrufen[0]
+    if gesamt_array_abrufen == []:
+        return False
+    else:
+        message_array_abrufen = gesamt_array_abrufen[0]
+        return message_array_abrufen
+
+
+def nachricht_senden(chatId, message):
+    requests.post(sendUrl + "?chat_id=" + str(chatId) + "&text="+ message) 
+
+
+
+
+
+while True:
+    willkomen(empfangURL)
+    sleep(1)
+
+
 
 
